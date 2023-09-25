610ee6c5b923c21a0ed75b3a8bb397729dc33ca8
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2023-09-25 11:46:43 +0200

    cleanup test

--- app/tests/unit/test_extract_csv_validator.py
+++ app/tests/unit/test_extract_csv_validator.py
@@ -36,34 +36,3 @@ def test_validation_methods(request: pytest.FixtureRequest, dataframe: pl.DataFr
     is_valid = method("%d/%m/%Y") if method_name == "is_date_range_valid" else method()
 
     assert is_valid is expected_result
-
-
-# @pytest.mark.parametrize(
-#     "dataframe, method_name, expected_result",
-#     [
-#         ("extract_places_df", "is_columns_valid", True),
-#         ("extract_places_df", "is_schema_valid", True),
-#         ("extract_places_df", "validate", True),
-#         ("invalid_extract_places_df", "is_columns_valid", False),
-#         ("invalid_extract_places_df", "is_schema_valid", False),
-#         ("invalid_extract_places_df", "validate", False)
-#     ]
-# )
-# def test_validation_methods(request: pytest.FixtureRequest, dataframe: pl.DataFrame, method_name: str, expected_result: bool):
-#     """This function test every validation method,
-#     from indivual methods (isolation) to interaction (orchestration) 
-
-#     Args:
-#         request (pytest.FixtureRequest): A request object gives access to the requesting test context
-#         dataframe (pl.DataFrame): dataframe to test
-#         method_name (str): method to test
-#         expected_result (bool): result expected
-#     """
-#     dataframe = request.getfixturevalue(dataframe)
-
-#     validator = ExtractCSVValidator(dataframe, "lieux-dits")
-#     method = getattr(validator, method_name)
-#     is_valid = method()
-
-#     assert is_valid == expected_result
-
